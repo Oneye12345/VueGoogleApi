bf1115f27eb63d877ffed7b5921b881a1484e115
Oneye12345 <kmanfool2@gmail.com>
2019-05-17 23:39:40 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="container">
+    <myMap />
+  </div>
+</template>
+
+<script>
+import myMap from './components/my-map.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+    
+  },
+  components: {
+    myMap
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/my-map.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="map container">
+    <img v-bind:src="url">
+</div>
+</template>
+
+<<script>
+export default {
+    name: 'map',
+        data () {
+            return {
+                url : 'https://maps.googleapis.com/maps/api/staticmap?center=34.0522,-118.2437&zoom=13&size=1000x1000&maptype=roadmap&markers=color:Red%7%7C34.0522,-118.2437&key=AIzaSyB0zhZrdrIkQRp1ViP9XjuwPqgTU6wY2sM'
+            }
+        }
+}
+</script>
